ea9f006418dccec2c646d7b08801a76785cf2aae
bvasilop <bvasilop@gmail.com>
2018-09-17 16:28:05 -0700

    variable object content and diagrams

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -2,6 +2,24 @@
 
 // A javascript engine is a program that executes javascript code (GoogleV8, SpiderMonkey)
 
+// Global Execution Context
+// --is for code that is not inside any function
+// --declaring a variable in the global object or window object in the browser window.
+// --lastName === window.lastName // true  - assigning a variable globally automatically
+// --applies it to the global window object
+
+// Properties are just variables attached to objects
+
+// Code that is inside a function
+// -- Each time we call a function, we get its own brand new execution context
+// -- var name = 'John' // global variable in a global context
+// -- function first() {
+//    var a = 'Hello';
+//    second();
+//    var x = a + name;
+//    } // global function in a global context
+// -- every time you call a new function, you get a new execution context
+// -- each time you execute a new function it is added to the top of the execution stack
 // Lecture: Hoisting
 
 
