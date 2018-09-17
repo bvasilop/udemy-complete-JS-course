f7474cad6758bf39f7436d9d5a8a714a7c87a103
bvasilop <bvasilop@gmail.com>
2018-09-16 20:57:04 -0700

    updated challenge 5 with comments

--- 2-JS-basics/coding-challenge-5/scripts.js
+++ 2-JS-basics/coding-challenge-5/scripts.js
@@ -34,28 +34,34 @@ GOOD LUCK 😀
 */
 'use strict';
 
-var john = {
+var john = { // start by creating john object literal
     fullName: 'John Smith',
-    bills : [124, 48, 268, 180, 42],
-    calcTips: function() {
-        this.tips = [];
-        this.finalValues = [];
+    bills : [124, 48, 268, 180, 42], // create array of bills
+    calcTips: function() { // calcTips method without any argument
+        this.tips = []; // start with empty array to fill later to our object
+        this.finalValues = []; // start with empty array to fill later to our object
 
-        for (var i = 0; i < this.bills.length; i++) {
+        for (var i = 0; i < this.bills.length; i++) { // loop through bills from array to calculate values.
+                                            // member access operator (this.bills.length -- the two dots)
+                                            // from precedence table. read from left to right.
+                                            // this.bills is replaced with bills array. Then we read the length
+                                            // property of the array
             // Determine percentage based on tipping rules
-            var percentage;
-            var bill =this.bills[i];
+            var percentage; // define percentage without assigning
+            var bill = this.bills[i]; // create new variable to store the current bill value so we don't repeat code this.bills[i]
 
-            if (bill < 50) {
+            if (bill < 50) { // if (this.bills[i] < 50) is our array with the bills so we assign it to var = bill; to make it easier to read
+                            // we access the current position of the array using the current counter variable which is [i]
                 percentage = 0.2;
-            } else if (bill >= 50 && bill < 200) {
+            } else if (bill >= 50 && bill < 200) { // if (this.bills[i] < 50 && this.bills[i] < 200)
                 percentage = 0.15;
             } else {
                 percentage = 0.1;
             }
             // add results to corresponding array
-            this.tips[i] = bill * percentage;
-            this.finalValues = bill + bill * percentage;
+            this.tips[i] = bill * percentage; // this.tips[i] adds the current position of [i].
+                        // bill variable defined from before * this.bills[i] (at the current position) * percentage
+            this.finalValues[i] = bill + bill * percentage;
 
         }
     }
@@ -80,26 +86,32 @@ var mark = {
                 percentage = 0.25;
             }
             // add results to corresponding array
-            this.tips[i] = bill * percentage;
-            this.finalValues[i] = bill + bill * percentage;
+            this.tips[i] = bill * percentage; // filling up empty arrays
+            this.finalValues[i] = bill + bill * percentage; // filling up empty arrays
 
         }
     }
 };
 
-function calcAverage(tips) {
-    var sum = 0;
-    for (var i = 0; i < tips.length; i++) {
-        sum = sum + tips[i];
-    }
-    return sum / tips.length;
+function calcAverage(tips) { // it's going to receive a tips array as an argument
+    var sum = 0; // loop over array and in each iteration store the current sum in a variable which starts from 0
+    for (var i = 0; i < tips.length; i++) { // loop over the tips array and then in each iteration add the current
+                                            // elements to the sum
+        sum = sum + tips[i]; // sum is = to the current sum + the tips at the current position [i]
+    } // say we have an array of [2,4,6], the begging of the array will be at 0 because sum = sum (which is 0 = tips[i])
+    return sum / tips.length; // when you have sum of the array, divide by the number of elements in it
 }
+
+/* why have a function for calcAverage instead of a method inside of the object ? Because then we would have to
+repeat ourselves by providing a method for each object*/
 
 // do the calculations
 john.calcTips();
 mark.calcTips();
+// console.log(john, mark); // calculate tips and log to console
 
-john.average = calcAverage(john.tips);
+john.average = calcAverage(john.tips); // calculate the average and then store them into the object and this
+                                    // is the array where all of the tips are stored in the average property on the john object
 mark.average = calcAverage(mark.tips);
 console.log(john, mark);
 
@@ -108,6 +120,8 @@ if (john.average > mark.average) {
 } else if (mark.average > john.average) {
     console.log(mark.fullName + '\'s family pays more tips, with an average of $' + mark.average);
 } // Mark Miller's family pays more tips, with an average of $32.2875
+
+// don't need an else statement because there is no default
 
 
 
