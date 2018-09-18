354b3082e3230c64f31047093db645b40ee8b2a2
bvasilop <bvasilop@gmail.com>
2018-09-18 11:30:13 -0700

    added DOM intro content

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -2,9 +2,21 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+/************
+ * DOM: Document Object Model
+ */
+
+// DOM is a structured representation of an HTML document
+// -- it is used to connect webpages to scripts like JavaScript
+// for each HTML box, there is an object in the DOM that we can access and interact with
+// DOM is a fully object-oriented representation. For each section (block) of our html code there is an
+// object in the DOM that we can access and interact with
+// using methods (functions attached to objects (document object) )to manipulate and interact with the DOM
+// HTML webpage content is stored in the DOM which can then be accessed and manipulated by javascript
