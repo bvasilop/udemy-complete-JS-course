cc39178da6d825356c1b9d7050d9aff37284530b
bvasilop <bvasilop@gmail.com>
2018-09-23 16:26:55 -0700

    added Object Oriented Programming content

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,20 @@
+// Everything is an Object: Inheritance and the Prototype Chain
+// Object -Oriented -Programming
+
+// Two types of Values : Primitives and Objects
+
+// Primitives: Numbers, Strings, Booleans, Undefined, Null
+
+// Everything else (is an object): Arrays, Functions, Objects, Dates, Wrappers for Numbers, Strings, Booleans
+
+// OOP : Objects interacting with one another through methods and properties.
+    // Used to store data, structure applications into modules and keeping code clean.
+
+// With help for Constructor functions, we can create as many instances of an object as we want.
+    // Constructor acts like a blueprint for other objects with the same properties
+
+// Inheritance : When one object is based on another object. When one object gets access to another objects
+    // properties and methods. One objects inherits these properties and methods from another object.
+
+// JS is a prototype based language. Inheritance works by using prototypes and the prototype chain.
+    // Each JS object has a prototype property which makes inheritance possible in JS.
