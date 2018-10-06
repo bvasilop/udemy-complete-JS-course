4d6191e24837284872dd4fb06d22993494b73c6f
bvasilop <bvasilop@gmail.com>
2018-10-05 20:56:28 -0700

    added IIFE to keep code private

--- 5-advanced-JS/Coding-Challenge-1/script.js
+++ 5-advanced-JS/Coding-Challenge-1/script.js
@@ -23,6 +23,9 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 'use strict';
+(function() { // IIFE
+
+
 function Question(question, answers, correct) {
         this.question = question;
         this.answers = answers;
@@ -62,7 +65,7 @@ var answer = parseInt(prompt('Please select the correct answer.')); // use parse
 
 questions[n].checkAnswer(answer);
 
-
+}) (); // IIFE
 
 
 
