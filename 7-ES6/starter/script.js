9d107e537598113ff6be71a93904c2c5b76b1c2d
bvasilop <bvasilop@gmail.com>
2018-10-13 20:54:13 -0700

    added arrow functions content

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -167,3 +167,109 @@ console.log(ages6); // (4) ["Age element 1: 28.", "Age element 2: 53.", "Age el
     // 1) with one argument and one line of code (simplest form)---first example
     // 2) with a second argument and use of parentheses ()---second example
     // 3) if we add more lines code (more than one) we add curly braces {} and return (keyword) statement at the end
+
+/*******************
+ * Arrow Functions 2
+ */
+
+// biggest advantage of using arrow functions is that they share the surrounding "this" keyword
+    // unlike normal functions, arrow functions don't get their own "this" keyword. They simply use the "this"
+        // keyword of the function they are written in (Lexical "this" variable).
+
+// ES 5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        var self = this; // using self to point to and store the 'this' variable and use in the rest of the function,
+            // fixes the pointing to global object problem (workaround/hack to avoid this situation) where in this function we don't have access to the box5 object
+                // why we use ES6 to avoid this hack
+        document.querySelector('.green').addEventListener('click', function() { // is actually a function pointing to the global (window) object not a method (pointing to the box 5 object)
+            var str = 'This box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+//box5.clickMe(); // This box number undefined and it is undefined (when using 'this') // This box number number 1 and it is green (when using 'self')
+
+// reason it is undefined is because it is not reading the values in our object. In only the method call the 'this' keyword
+    // actually points to that object, but in a regular function call, the 'this' keyword will always point to the global object.
+        // which in the case of the browser, is the window object.
+
+// The click me method is a function attached to an object (method)
+
+// ES 6
+
+// With arrow functions we share the surrounding 'this' keyword
+
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() { // can be replaced with arrow function (see ex below)
+
+        document.querySelector('.green').addEventListener('click', () => { // we use () and => because we are using more than one argument
+            let str = 'This box number ' + this.position + ' and it is ' + this.color; // in here we have a function that shares
+            // the lexical 'this' keyword with its surroundings. The 'this' keyword now points to the object
+            alert(str);
+        });
+    }
+}
+box6.clickMe(); // This box number 1 and it is green
+
+// It is best practice to always use arrow functions when you need to preserve the values of the 'this' keyword (like in this ex) but beware
+    // if the 'this' keyword points to the global object instead of intended object when using arrow functions
+
+/*
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => { // this method now shares the lexical 'this' keyword with its surroundings (global context) (undefined errors like ex 1)
+        // no longer has its own 'this' keyword. Shares the global 'this' keyword (window object)
+
+        document.querySelector('.green').addEventListener('click', () => { // we use () and => because we are using more than one argument
+            let str = 'This box number ' + this.position + ' and it is ' + this.color; // in here we have a function that shares
+            // the lexical 'this' keyword with its surroundings. The 'this' keyword now points to the object
+            alert(str);
+        });
+    }
+}
+box66.clickMe(); // This box number undefined and it is undefined
+*/
+
+// Using function constructor with arrow functions
+
+function Person(name) {
+    this.name = name;
+// add a method to the prototype property of the person so the object created through
+    // the Person function constructor inherits this method
+}
+// ES 5
+Person.prototype.myFriends5 = function(friends) {
+    // var self = this; // can use same hack as before storing the 'this' variable in self to point to the object
+
+    // we can also use call, bind and apply
+    var arr = friends.map(function(el) { // we get undefined error for name because as before
+            // the 'this' variable points to the global window object
+        return this.name + ' is friends with ' + el; // el is the current element
+    }.bind(this)); // we can create a copy of this function with the bind method and pass a manually defined 'this' keyword into it which now points to the new Person
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+
+// ES 6
+
+Person.prototype.myFriends6 = function(friends) {
+
+
+
+    const arr = friends.map(el => `${this.name} is friends with ${el}`);
+    // we share the lexical 'this' keyword with our surroundings (the method above)
+
+    console.log(arr);
+};
+
+new Person('Mike').myFriends6(friends);
