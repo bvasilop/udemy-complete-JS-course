f83d77c5e1e590562320899f863b09bf5712ab7b
bvasilop <bvasilop@gmail.com>
2018-11-05 19:51:31 -0800

    added todo for budgety app

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,17 @@
+// Using modules to assist with the architecture of the app
+    // keeps project cleanly separated and organized
+    // encapsulates data into privacy and expose other data publicly
+
+// TODO list
+
+// UI Module
+    // get input values
+    // add new item to UI
+    // Update UI
+
+// Data Module
+    // Add new item to our data structure
+    // Calculate budget
+
+// Controller Module
+    // Add event handler
