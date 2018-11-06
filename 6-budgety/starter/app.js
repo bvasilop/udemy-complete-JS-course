4b091ce186a19886cc8edf0c7151096ff8d1e4ac
bvasilop <bvasilop@gmail.com>
2018-11-05 20:20:03 -0800

    added modules for architecture of app

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -15,3 +15,48 @@
 
 // Controller Module
     // Add event handler
+
+// Module Pattern
+
+/* public methods, which means that we expose them to the public so that other functions
+or modules can access and use them. And this is called data encapsulation.
+So data encapsulation allows us to hide the implementation details of a specific module
+from the outside scope so that we only expose a public interface which is sometimes called an API. */
+
+// Budget Controller Module
+
+var budgetController = (function() {
+    var x = 23;
+
+    var add = function(a) { // not accessible outside of IFFY
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) { // the variables above are available and  public because of closures
+            return add(b);
+        }
+    }
+})();
+
+// UI Controller Module
+
+var UIController = (function() {
+    // some code3
+
+})();
+
+
+// App Controller Module
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
